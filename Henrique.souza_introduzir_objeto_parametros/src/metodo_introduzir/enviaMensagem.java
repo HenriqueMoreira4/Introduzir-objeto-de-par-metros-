ee75713f7f8f81d71f3a439ffc1c2f2cd25f0358
Henrique Gabriel Moreira Souza <79548206+HenriqueMoreira4@users.noreply.github.com>
2022-05-03 17:39:10 -0300

    Add files via upload

---
+++ Henrique.souza_introduzir_objeto_parametros/src/metodo_introduzir/enviaMensagem.java
@@ -0,0 +1,12 @@
+package metodo_introduzir;
+
+public class enviaMensagem {
+	public void enviaMensagem() {
+		System.out.print("bem vindo ao sisteminha ruim. ");
+	}
+	public void boasVindas(String nome, int idade) {
+		System.out.printf("Olá, %s, você tem %d anos de idade?", nome, idade);
+	}
+	
+
+}
---
+++ Henrique.souza_introduzir_objeto_parametros/src/metodo_introduzir/principal.java
@@ -0,0 +1,27 @@
+package metodo_introduzir;
+import java.util.Scanner;
+import java.util.Scanner;
+
+
+public class principal {
+	
+	public static void main(String[] args) {
+		String nomeAluno;
+		int idadeAluno;
+		Scanner entrada = new Scanner(System.in);
+		
+		enviaMensagem oi = new enviaMensagem();
+		
+		System.out.println("Qual seu nome? ");
+		nomeAluno = entrada.nextLine();
+		
+		System.out.println("Qual sua idade? ");
+		idadeAluno = entrada.nextInt();
+		
+		oi.boasVindas(nomeAluno, idadeAluno);
+		
+		entrada.close();
+		oi.enviaMensagem();
+	}
+
+}
